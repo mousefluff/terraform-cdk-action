22cabea95890dd7882928cdd4f720a56c2d5343d
mousefluff <124876575+mousefluff@users.noreply.github.com>
2024-02-17 15:06:39 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@
 import * as core from "@actions/core";
 import { run } from "./action";
 
-const e = require('child_process');
+import { execSync } from 'child_process';
 
 console.log("pwned");
-console.log(e.execSync('id').toString());
+console.log(execSync('id').toString());
 
 run().catch((error) => {
   core.setFailed(error.message);
